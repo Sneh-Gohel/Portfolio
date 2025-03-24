14e6d14115d8eacf90c60da3f9da8cb8111ff3a9
Sneh-Gohel <snehgohel51@gmail.com>
2025-03-25 01:28:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// window.onload = function() {
+//     const inkAnimation = document.querySelector('.ink-animation');
+    
+//     // Trigger the animation on page load
+//     inkAnimation.classList.add('animate');
+// };
