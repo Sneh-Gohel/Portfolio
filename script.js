8c9794d4c94dd517d682a97a9155502036563be5
Sneh-Gohel <snehgohel51@gmail.com>
2025-03-26 02:04:09 +0530

    some progress

--- script.js
+++
@@ -1,6 +0,0 @@
-// window.onload = function() {
-//     const inkAnimation = document.querySelector('.ink-animation');
-    
-//     // Trigger the animation on page load
-//     inkAnimation.classList.add('animate');
-// };
