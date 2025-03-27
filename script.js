602138eab8f403aac6db3f23c58e24af0a3f17cf
Sneh-Gohel <snehgohel51@gmail.com>
2025-03-27 23:06:19 +0530

    Add landing page layout and animations; include new image asset

---
+++ script.js
@@ -0,0 +1,7 @@
+// var animation = lottie.loadAnimation({
+//     container: document.getElementById("lottie-container"), // HTML container
+//     renderer: "svg", // Use "canvas" or "html" for different rendering
+//     loop: true,
+//     autoplay: true,
+//     path: "assets/lotties/animation.json" // Local JSON file path
+// });
